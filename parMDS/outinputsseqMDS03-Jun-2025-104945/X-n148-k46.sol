ca863aa3892799940ca3ca5d705892e1d919fbb3
Chekkala Sandeep Reddy <sandeep.chekkala.wh@gmail.com>
2025-06-03 11:22:08 +0530

    parMDS

---
+++ parMDS/outinputsseqMDS03-Jun-2025-104945/X-n148-k46.sol
@@ -0,0 +1,53 @@
+Route #1: 99 42
+Route #2: 75 88
+Route #3: 44
+Route #4: 106 129
+Route #5: 54 68 14 67
+Route #6: 98 57 7 61 10
+Route #7: 18 136
+Route #8: 82 32 104
+Route #9: 137 110 85
+Route #10: 127 77 142 109
+Route #11: 17 112
+Route #12: 35 70
+Route #13: 113 79 92 123
+Route #14: 128 146
+Route #15: 96 91
+Route #16: 139 34
+Route #17: 134 33 13 66 36
+Route #18: 107 62 118
+Route #19: 84 122
+Route #20: 2 111 27
+Route #21: 22 55 49 145 86
+Route #22: 46 47 23
+Route #23: 126 73 39
+Route #24: 43 58
+Route #25: 21 124 72
+Route #26: 93 83
+Route #27: 140 121
+Route #28: 143 132
+Route #29: 97 28
+Route #30: 56 30 45
+Route #31: 60 94 138 131 102
+Route #32: 16 12 117
+Route #33: 3 74
+Route #34: 90 116
+Route #35: 100 6 115 135
+Route #36: 105 40
+Route #37: 20 120 80 101 52
+Route #38: 81 119
+Route #39: 103 48 108 15
+Route #40: 114 31 8 125 37
+Route #41: 26 130
+Route #42: 53 4 64 19
+Route #43: 65 144
+Route #44: 41 78
+Route #45: 133 141 147
+Route #46: 51 50
+Route #47: 69 63 24 9 11 38
+Route #48: 29 25
+Route #49: 5 1
+Route #50: 71 59 76
+Route #51: 95 87
+Route #52: 89
+Cost 48344
---
+++ parMDS/outinputsseqMDS_bfs03-Jun-2025-111739/X-n172-k51.sol
@@ -0,0 +1,64 @@
+Route #1: 130 143
+Route #2: 136 129 65
+Route #3: 150 169 86
+Route #4: 124 51
+Route #5: 153 80 77
+Route #6: 156 35 67
+Route #7: 137 100
+Route #8: 42 101
+Route #9: 134
+Route #10: 167 171
+Route #11: 115 151 104
+Route #12: 147 68 48
+Route #13: 170 6 40 22 13
+Route #14: 135 43 49 61
+Route #15: 113 15 1 17
+Route #16: 18 95 73 140
+Route #17: 154 63 90
+Route #18: 44 19 128 166
+Route #19: 36
+Route #20: 148 79
+Route #21: 72 82 3
+Route #22: 125
+Route #23: 117 30 56 45
+Route #24: 54 118 162
+Route #25: 71 66 31 14
+Route #26: 27
+Route #27: 2 39
+Route #28: 4
+Route #29: 34 133
+Route #30: 52
+Route #31: 109 78 20
+Route #32: 33 111 12
+Route #33: 24 75
+Route #34: 74 81
+Route #35: 76 84
+Route #36: 97
+Route #37: 38 64 29
+Route #38: 57 55
+Route #39: 41 58
+Route #40: 59 8 9
+Route #41: 26
+Route #42: 85 50
+Route #43: 126 5 139
+Route #44: 163 157 10
+Route #45: 165
+Route #46: 47
+Route #47: 46 160
+Route #48: 23 168 144
+Route #49: 120 123
+Route #50: 145 116
+Route #51: 21 146
+Route #52: 138 159
+Route #53: 88 25
+Route #54: 60 83
+Route #55: 102 28
+Route #56: 69 141 53 11
+Route #57: 132 161 62 152 112
+Route #58: 131 87 70 114 110 92
+Route #59: 103 155 149 107 99 37
+Route #60: 105 119 94 142 164 158 108
+Route #61: 121 96 89 127 7
+Route #62: 32 91 93 98 122
+Route #63: 16 106
+Cost 67188
